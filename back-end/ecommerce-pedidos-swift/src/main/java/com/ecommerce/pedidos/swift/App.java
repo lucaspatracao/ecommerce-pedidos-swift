4d8c46b393512bc611c1db3f19a1262fef159787
Lucas Nunes Patracao <lucas.n.patracao@gmail.com>
2026-08-07 22:46:00 -0300

    Chore: Configuração do Ambiente Back-End

---
+++ back-end/ecommerce-pedidos-swift/src/main/java/com/ecommerce/pedidos/swift/App.java
@@ -0,0 +1,10 @@
+package com.ecommerce.pedidos.swift;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
